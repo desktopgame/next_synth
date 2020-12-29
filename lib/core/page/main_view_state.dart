9bd8ef9aeeaa8f167e5802853c00d74014d32b0b
Koya <koya0306@outlook.jp>
2020-12-29 09:36:23 +0900

    インポートを修正

--- lib/core/page/main_view_state.dart
+++ lib/core/page/main_view_state.dart
@@ -1,27 +1,13 @@
 import 'package:flutter/material.dart';
-import '../../piano_roll/main_view.dart';
-import '../../piano_roll/track_list.dart';
-import '../../piano_roll/piano_roll_editor.dart';
+import './main_view.dart';
+import 'package:next_synth/piano_roll/track_list.dart';
+import 'package:next_synth/piano_roll/piano_roll_editor.dart';
 
 class MainViewState extends State<MainView> {
   @override
   Widget build(BuildContext context) {
-    //*
     return Row(
       children: [TrackList(), Expanded(child: PianoRollEditor())],
     );
-
-    //  */
-/*
-    return Stack(
-      children: [
-        Align(alignment: Alignment.centerRight, child: PianoRollEditor()),
-        Align(
-          alignment: Alignment.topLeft,
-          child: TrackList(),
-        ),
-      ],
-    );
-    */
   }
 }
