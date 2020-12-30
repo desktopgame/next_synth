2b3e3cb786d1ae95581c17b5a71ba2ea5ccff5da
Koya <koya0306@outlook.jp>
2020-12-30 13:53:47 +0900

    プロジェクト名入力処理実装

--- lib/core/page/start_page_state.dart
+++ lib/core/page/start_page_state.dart
@@ -3,6 +3,7 @@ import './start_page.dart';
 import '../system/project.dart';
 import '../system/project_list.dart';
 import '../system/project_list.save_data.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 
 class StartPageState extends State<StartPage> {
   int _selectedProjectIndex;
@@ -13,11 +14,91 @@ class StartPageState extends State<StartPage> {
     this._selectedProjectIndex = -1;
   }
 
-  List<Widget> _buildFooterButtons() {
+  static void _showConfirmDialog(BuildContext context, String placeholder,
+      {String approveText = "OK",
+      String cancelText = "Cancel",
+      Color color = Colors.lightBlue,
+      void Function(TextEditingController controller) onApprrove,
+      void Function(TextEditingController controller) onCancel}) {
+    var controller = TextEditingController();
+    showDialog(
+        child: new Dialog(
+          backgroundColor: Colors.transparent,
+          insetPadding: EdgeInsets.all(10),
+          child: Stack(
+            overflow: Overflow.visible,
+            alignment: Alignment.center,
+            children: [
+              SingleChildScrollView(
+                  child: Container(
+                width: double.infinity,
+                height: 200,
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(15), color: color),
+                padding: EdgeInsets.fromLTRB(20, 50, 20, 20),
+                child: new Column(
+                  children: <Widget>[
+                    new TextField(
+                      decoration: new InputDecoration(hintText: placeholder),
+                      controller: controller,
+                    ),
+                    Row(children: [
+                      Spacer(),
+                      new FlatButton(
+                        child: new Text(cancelText),
+                        onPressed: () {
+                          onCancel(controller);
+                        },
+                      ),
+                      new FlatButton(
+                        child: new Text(approveText),
+                        onPressed: () {
+                          onApprrove(controller);
+                        },
+                      ),
+                    ]),
+                  ],
+                ),
+              ))
+            ],
+          ),
+        ),
+        context: context);
+  }
+
+  void _newProject(String name) {
+    if (name == null || name.length == 0) {
+      Fluttertoast.showToast(
+          msg: "プロジェクト名が空です。",
+          toastLength: Toast.LENGTH_SHORT,
+          gravity: ToastGravity.CENTER,
+          timeInSecForIosWeb: 1,
+          backgroundColor: Colors.red,
+          textColor: Colors.white,
+          fontSize: 16.0);
+      return;
+    }
+    setState(() {
+      var projectList = ProjectListProvider.provide().value;
+      projectList.data.add(Project()..name = name);
+    });
+  }
+
+  void _deleteProject() {}
+
+  List<Widget> _buildFooterButtons(BuildContext context) {
     return <Widget>[
       IconButton(
         icon: Icon(Icons.add),
-        onPressed: () => {},
+        onPressed: () {
+          _showConfirmDialog(context, "プロジェクト名",
+              onApprrove: (TextEditingController controller) {
+            Navigator.pop(context);
+            _newProject(controller.text);
+          }, onCancel: (TextEditingController controller) {
+            Navigator.pop(context);
+          });
+        },
       ),
       IconButton(
         icon: Icon(Icons.delete),
@@ -33,13 +114,13 @@ class StartPageState extends State<StartPage> {
   @override
   Widget build(BuildContext context) {
     var projects = ProjectListProvider.provide().value.data;
-    var footerButtons = _buildFooterButtons();
+    var footerButtons = _buildFooterButtons(context);
     // プロジェクトが一つもない
     if (projects.length == 0) {
       return Scaffold(
           persistentFooterButtons: footerButtons,
           body: Center(
-            child: Text("プロジェクトがありません。"),
+            child: Text("プロジェクトがありません。", textAlign: TextAlign.center),
           ));
     }
     // プロジェクト一覧の表示
