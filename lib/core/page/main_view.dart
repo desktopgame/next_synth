28ede065de99b0e9bdb543a2ed2eb2d1e87676c6
Koya <koya0306@outlook.jp>
2021-01-01 18:23:30 +0900

    編集中のプロジェクトを指定するインデックスを受け取れるように

--- lib/core/page/main_view.dart
+++ lib/core/page/main_view.dart
@@ -2,8 +2,12 @@ import 'package:next_synth/core/page/main_view_state.dart';
 import 'package:flutter/material.dart';
 
 class MainView extends StatefulWidget {
+  int _projectIndex;
+
+  MainView(this._projectIndex);
+
   @override
   State<StatefulWidget> createState() {
-    return MainViewState();
+    return MainViewState(_projectIndex);
   }
 }
--- lib/core/page/main_view_state.dart
+++ lib/core/page/main_view_state.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:next_synth/core/system/project_list.save_data.dart';
 import './main_view.dart';
 import 'package:next_synth/piano_roll/track_list.dart';
 import 'package:next_synth/piano_roll/piano_roll_editor.dart';
@@ -7,17 +8,23 @@ import 'package:next_synth/piano_roll/piano_roll_model.dart';
 import 'package:next_synth/piano_roll/default_piano_roll_model.dart';
 import 'package:next_synth/piano_roll/piano_roll_style.dart';
 import 'package:next_synth/piano_roll/piano_roll_layout_info.dart';
+import 'package:logging/logging.dart';
 
 class MainViewState extends State<MainView> {
+  int _projectIndex;
   PianoRollModel model;
   PianoRollStyle style;
   PianoRollLayoutInfo layoutInfo;
+  final logger = new Logger('MainViewState');
 
-  MainViewState() {}
+  MainViewState(this._projectIndex) {}
 
   @override
   void initState() {
     super.initState();
+    final projList = ProjectListProvider.provide().value;
+    final proj = projList.data[_projectIndex];
+    debugPrint("MainViewState: ${_projectIndex}=${proj.name}");
     this.model = DefaultPianoRollModel(11 * 12, 4, 4);
     this.style = PianoRollStyle();
     this.layoutInfo = PianoRollLayoutInfo();
