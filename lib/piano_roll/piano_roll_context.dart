01af2eb125c3506540b18fb705df02908a7844ab
Koya <koya0306@outlook.jp>
2021-01-23 20:04:21 +0900

    スクロール処理微修正

--- lib/piano_roll/piano_roll_context.dart
+++ lib/piano_roll/piano_roll_context.dart
@@ -153,6 +153,7 @@ class PianoRollContext implements PianoRollUI {
     var mh = computeMaxHeight();
     var parH = height / mh;
     offset = offset.translate(0, -((range.scrollOffset.dy * parH)));
-    return Rect.fromLTRB(left, offset.dy, right, offset.dy + (parH * height));
+    return Rect.fromLTRB(
+        left, offset.dy, right, (offset.dy - 50) + (parH * height));
   }
 }
