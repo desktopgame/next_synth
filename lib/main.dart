19426754e8309e04c22904a2df337e7f3d576b36
koya <gnndr36@gmail.com>
2021-03-13 16:07:54 +0900

    ダークテーマ切り替え可能に

--- lib/main.dart
+++ lib/main.dart
@@ -47,35 +47,69 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner:
-          AppDataProvider.provide().value.showDebugLabel,
-      title: 'NextSynth',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-        // This makes the visual density adapt to the platform that you run
-        // the app on. For desktop platforms, the controls will be smaller and
-        // closer together (more dense) than on mobile platforms.
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
-      initialRoute: "/",
-      routes: <String, WidgetBuilder>{
-        "/": (BuildContext context) => StartPage(),
-        "/project": (BuildContext context) => ProjectSettingPage(),
-        "/edit": (BuildContext context) => EditPage(),
-        "/setting": (BuildContext context) => SettingPage(),
-        "/usb": (BuildContext context) => USBInfoPage(),
-        "/code": (BuildContext context) => CodePage()
-      },
-    );
+    if (AppDataProvider.provide().value.darkTheme) {
+      return MaterialApp(
+        debugShowCheckedModeBanner:
+            AppDataProvider.provide().value.showDebugLabel,
+        title: 'NextSynth',
+        theme: ThemeData(
+          // This is the theme of your application.
+          //
+          // Try running your application with "flutter run". You'll see the
+          // application has a blue toolbar. Then, without quitting the app, try
+          // changing the primarySwatch below to Colors.green and then invoke
+          // "hot reload" (press "r" in the console where you ran "flutter run",
+          // or simply save your changes to "hot reload" in a Flutter IDE).
+          // Notice that the counter didn't reset back to zero; the application
+          // is not restarted.
+          primarySwatch: Colors.blue,
+          // This makes the visual density adapt to the platform that you run
+          // the app on. For desktop platforms, the controls will be smaller and
+          // closer together (more dense) than on mobile platforms.
+          visualDensity: VisualDensity.adaptivePlatformDensity,
+        ),
+        darkTheme: ThemeData.dark(),
+        initialRoute: "/",
+        routes: <String, WidgetBuilder>{
+          "/": (BuildContext context) => StartPage(),
+          "/project": (BuildContext context) => ProjectSettingPage(),
+          "/edit": (BuildContext context) => EditPage(),
+          "/setting": (BuildContext context) => SettingPage(),
+          "/usb": (BuildContext context) => USBInfoPage(),
+          "/code": (BuildContext context) => CodePage()
+        },
+      );
+    } else {
+      return MaterialApp(
+        debugShowCheckedModeBanner:
+            AppDataProvider.provide().value.showDebugLabel,
+        title: 'NextSynth',
+        theme: ThemeData(
+          // This is the theme of your application.
+          //
+          // Try running your application with "flutter run". You'll see the
+          // application has a blue toolbar. Then, without quitting the app, try
+          // changing the primarySwatch below to Colors.green and then invoke
+          // "hot reload" (press "r" in the console where you ran "flutter run",
+          // or simply save your changes to "hot reload" in a Flutter IDE).
+          // Notice that the counter didn't reset back to zero; the application
+          // is not restarted.
+          primarySwatch: Colors.blue,
+          // This makes the visual density adapt to the platform that you run
+          // the app on. For desktop platforms, the controls will be smaller and
+          // closer together (more dense) than on mobile platforms.
+          visualDensity: VisualDensity.adaptivePlatformDensity,
+        ),
+        initialRoute: "/",
+        routes: <String, WidgetBuilder>{
+          "/": (BuildContext context) => StartPage(),
+          "/project": (BuildContext context) => ProjectSettingPage(),
+          "/edit": (BuildContext context) => EditPage(),
+          "/setting": (BuildContext context) => SettingPage(),
+          "/usb": (BuildContext context) => USBInfoPage(),
+          "/code": (BuildContext context) => CodePage()
+        },
+      );
+    }
   }
 }
