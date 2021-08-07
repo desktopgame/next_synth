fa5e14c9335eca583a8123a37f41d99fcc728931
koya <gnndr36@gmail.com>
2021-08-07 21:16:11 +0900

    不要なコードを削除

--- lib/core/page/edit_page_state.dart
+++ lib/core/page/edit_page_state.dart
@@ -8,16 +8,9 @@ class EditPageState extends State<EditPage> {
   Widget build(BuildContext context) {
     final projectIndex = ModalRoute.of(context).settings.arguments as int;
     return Scaffold(
-      //appBar: AppBar(
-      //  // Here we take the value from the MyHomePage object that was created by
-      //  // the App.build method, and use it to set our appbar title.
-      //  title: Text(widget.title),
-      //),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: MainView(projectIndex),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      ),
     );
   }
 }
